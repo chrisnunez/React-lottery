f79bfe78e57cf945add8cf7cfd74e08a493befca
Chrismilez <nunezchristian92@gmail.com>
2022-09-01 19:17:06 -0700

    added logic for lottery

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Ball from './Ball';
+import Lottery from './Lottery';
+
 function App() {
   return (
     <div className="App">
-     <Ball num={17} />
+     <Lottery />
+     <Lottery title='Mini Daily' maxNum={10} maxBalls={4} />
     </div>
   );
 }
